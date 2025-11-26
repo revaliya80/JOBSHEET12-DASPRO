01210fa6f6d9c9ada06e86140c6cdf4b395b3f02
Refaliya-Vinandha <revaliya80@gmail.com>
2025-11-26 21:05:27 +0700

    Percobaan 2

--- Kafe24.java
+++ Kafe24.java
@@ -1,5 +1,11 @@
 public class Kafe24 {
-    public static void main(String[] args) {
+    public static void Menu(String namaPelanggan, boolean isMember) {
+        System.out.println("Selamat datang, " + namaPelanggan + ":");
+
+        if (isMember) {
+            System.out.println("Anda adalah member, dapatkan diskon 10% untuk setiap pembelian!");
+        }
+
         System.out.println("===== MENU RESTO KAFE =====");
         System.out.println("1. Kopi Hitam - Rp 15.000");
         System.out.println("2. Cappuccino - Rp 20.000");
@@ -9,5 +15,10 @@ public class Kafe24 {
         System.out.println("6. Mie Goreng - Rp 18.000");
         System.out.println("=============================");
         System.out.println("Silakan pilih menu yang Anda inginkan.");
+    }
+
+    public static void main(String[] args) {
+        Menu("Andi", true);
+
     }
 }
